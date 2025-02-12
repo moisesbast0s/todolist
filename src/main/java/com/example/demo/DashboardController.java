3fce0266827c917d124d5d28d0d54b20ae91f030
Moises Bastos <moisesbastos8@gmail.com>
2025-02-12 09:33:49 -0300

    Alteraçoes

--- src/main/java/com/example/demo/DashboardController.java
+++ src/main/java/com/example/demo/DashboardController.java
@@ -1,6 +1,4 @@
 package com.example.demo;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller	  
 public class DashboardController {
 
     @Autowired
