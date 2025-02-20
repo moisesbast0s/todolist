72f248da8a48919ffbf505c561a6c0903aca8e4e
Moises Bastos <moisesbastos8@gmail.com>
2025-02-20 17:24:42 -0300

    Novas funcionalidades criadas

---
+++ src/main/java/com/example/demo/models/UsuarioValid.java
@@ -0,0 +1,44 @@
+package com.example.demo.models;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UsuarioValid{
+    
+    @NotBlank(message = "O nome não pode estar vazio")
+    private String nome;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "O email é obrigatório")
+    private String email;
+
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    private String senha;
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
