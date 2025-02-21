b3992e2bb35286184d93c45bfb4887f0f3ca6ce1
Moises Bastos <moisesbastos8@gmail.com>
2025-02-21 13:54:59 -0300

    .

--- src/main/java/com/example/demo/models/Usuario.java
+++ src/main/java/com/example/demo/models/Usuario.java
@@ -4,9 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,14 +16,10 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @NotBlank(message = "O nome não pode estar vazio")
     private String nome;
     
-    @Email(message = "Email inválido")
-    @NotBlank(message = "O email é obrigatório")
     private String email;
     
-    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
     private String senha;
     
 }
